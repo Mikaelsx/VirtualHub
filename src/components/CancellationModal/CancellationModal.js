bb18ebd70c3fbb0e8a5a031439633802354fe975
Mikael Souza Da França Falcão <51915337801@info.local>
2024-03-18 16:12:18 -0300

    18/03

--- src/components/CancellationModal/CancellationModal.js
+++ src/components/CancellationModal/CancellationModal.js
@@ -4,7 +4,46 @@ import { ModalContent, ModalText, PatientModal } from "./style";
 import { ButtonSecundaryTitle, ButtonTitle } from "../ButtonTitle/style";
 import { ButtonModal, ButtonSecondary } from "../Button/style";
 
+import * as Notifications from 'expo-notifications' // EXPO NOTIFICATIONS
+
+// SOLICITA PERMISSÕES DE NOTIFICAÇÃO AO INICIAR O APP
+Notifications.requestPermissionsAsync(); 
+
+// DEFINE COMO AS NOTIFICAÇÕES DEVEM SER TRATADAS QUANDO RECEBIDAS
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    // MOSTRAR O ALERTA QUANDO A NOTIFICAÇÃO FOR RECEBIDA
+    shouldShowAlert : true,
+    // REPRODUS SOM AO RECEBER NOTIFICAÇÃO
+    shouldPlaySound : false,
+    // NÚMERO DE NOTIFICAÇÕES NO ÍCONE DO APP
+    shouldSetBadge : false,
+  })
+})
+
 export const CancelattionModal = ({ visible, setShowModalCancel, ...rest }) => {
+
+  // FUNÇÃO PARA LIDAR COM CHAMADA DE NOTIFICAÇÃO
+  const handleCallNotifications = async () => {
+    // OBTEM STATUS DA PERMISSÃO
+    const {status} = await Notifications.getPermissionsAsync();
+    // VERIFICA SE O USUÁRIO CONCEDEU PERMISSÃO
+    if (status !== "granted") {
+      alert("Você não deixou as notificações ativas.")
+      return;
+    }
+
+    // AGENDA UMA NOTIFICAÇÃO 
+    await Notifications.scheduleNotificationAsync({
+      content : {
+        title : "Consulta Cancelada",
+        body : "Notificação recebida.",
+        color: '#49B3BA'
+      },
+      trigger : null,
+    })
+  }
+
   return (
     <Modal {...rest} visible={visible} transparent={true} animationType="fade">
       <PatientModal>
@@ -16,7 +55,7 @@ export const CancelattionModal = ({ visible, setShowModalCancel, ...rest }) => {
             seu horário, deseja mesmo cancelar essa consulta?
           </ModalText>
 
-          <ButtonModal>
+          <ButtonModal onPress={handleCallNotifications}>
             <ButtonTitle>Confirmar</ButtonTitle>
           </ButtonModal>
 
